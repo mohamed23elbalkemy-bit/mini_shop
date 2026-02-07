526f63a3ad11d4726d9b0035de0b1e044f371031
Mohamed Nasr <mohamed23elbalkemy@gmail.com>
2026-02-07 19:11:54 +0200

    add product data model

---
+++ lib/models/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String id;
+  final String title;
+  final double price;
+  final String image;
+  final double rating;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.image,
+    required this.rating,
+  });
+}
